38bb3f6ec860ee4953b4d27026a5a6c40002a41c
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-01-09 23:55:58 -0300

    implement local storage

---
+++ shared/src/iosMain/kotlin/io/github/alaksion/unsplashwrapper/localstorage/IosLocalStorage.kt
@@ -0,0 +1,19 @@
+package io.github.alaksion.unsplashwrapper.localstorage
+
+import platform.Foundation.NSUserDefaults
+import platform.Foundation.setValue
+
+actual class LocalStorageImpl : LocalStorage {
+
+    private val userDefaults by lazy {
+        NSUserDefaults.standardUserDefaults()
+    }
+
+    override fun putString(value: String, key: String) {
+        userDefaults.setValue(value = value, forUndefinedKey = key)
+    }
+
+    override fun getString(key: String): String? {
+        return userDefaults.stringForKey(key)
+    }
+}
