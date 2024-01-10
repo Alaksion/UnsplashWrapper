fa623d5a912bd566d341f5cfed16eac9004cbb54
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-01-10 00:03:15 -0300

    feat: create sdk file

---
+++ shared/src/commonMain/kotlin/io/github/alaksion/unsplashwrapper/sdk/UnsplashWrapperSdk.kt
@@ -0,0 +1,13 @@
+package io.github.alaksion.unsplashwrapper.sdk
+
+class UnsplashWrapperSdk private constructor() {
+
+    fun initialize(apiKey: String) {
+
+    }
+
+    companion object {
+        val Instance = UnsplashWrapperSdk()
+    }
+
+}
