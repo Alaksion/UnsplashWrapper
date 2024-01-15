a389246bf3054fb113c759a82f119c1a24b909f5
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-01-15 00:26:13 -0300

    feat: remove ios sample tests

--- shared/src/iosTest/kotlin/io/github/alaksion/unsplashwrapper/Test.ios.kt
+++ shared/src/iosTest/kotlin/io/github/alaksion/unsplashwrapper/Test.ios.kt
@@ -1,12 +1,5 @@
 package io.github.alaksion.unsplashwrapper
 
-import kotlin.test.Test
-import kotlin.test.assertTrue
-
 class IosGreetingTest {
 
-    @Test
-    fun testExample() {
-        assertTrue(Greeting().greet().contains("iOS"), "Check iOS is mentioned")
-    }
 }
