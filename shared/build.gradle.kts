ad9ba1ac885e511076e424428fd685ac561c531b
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-01-15 01:02:30 -0300

    feat: config maven publication pom

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -78,11 +78,31 @@ detekt {
 }
 
 publishing {
-    repositories {
-        mavenCentral {
-            group = "io.github.alaksion"
-            name = "unsplash-wrapper"
-            version = "0.0.1"
+    publications {
+
+        create<MavenPublication>("mavenPub") {
+            pom {
+                name = "unsplash-wrapper"
+                description =
+                    "Wrapper SDK of the Unsplash public API. https://unsplash.com/documentation#creating-a-developer-account"
+                version = "0.0.1"
+                group = "io.github.alaksion"
+                artifactId = "unsplash-wrapper"
+                licenses {
+                    licenses {
+                        name = "MIT License"
+                        url = "https://opensource.org/license/mit/"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "Alaksion"
+                        name = "Lucca Beurmann"
+                        email = "lbeurmann.dev@gmail.com"
+                    }
+                }
+            }
         }
+
     }
 }
