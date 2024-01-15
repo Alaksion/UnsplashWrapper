200ce4262f00bc18c48b17bd1e449f0bfa0a722e
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-01-15 01:15:51 -0300

    chore: fix detekt

--- shared/src/commonMain/kotlin/io/github/alaksion/unsplashwrapper/sdk/UnsplashSdkConfig.kt
+++ shared/src/commonMain/kotlin/io/github/alaksion/unsplashwrapper/sdk/UnsplashSdkConfig.kt
@@ -2,10 +2,10 @@ package io.github.alaksion.unsplashwrapper.sdk
 
 internal object UnsplashSdkConfig {
 
-    private val baseUrl = "https://api.unsplash.com"
+    private const val baseUrl = "https://api.unsplash.com"
 
     fun buildUrl(url: String) = "$baseUrl/$url"
 
-    val version = "v1"
+    const val version = "v1"
 
 }
