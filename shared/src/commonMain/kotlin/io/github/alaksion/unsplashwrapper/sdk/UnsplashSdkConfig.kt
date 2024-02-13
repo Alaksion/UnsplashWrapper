777ae27a8bf63408f2ba975e2e94c7131a92872f
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-02-13 14:06:41 -0300

    feat: map authorization data source

--- shared/src/commonMain/kotlin/io/github/alaksion/unsplashwrapper/sdk/UnsplashSdkConfig.kt
+++ shared/src/commonMain/kotlin/io/github/alaksion/unsplashwrapper/sdk/UnsplashSdkConfig.kt
@@ -3,6 +3,7 @@ package io.github.alaksion.unsplashwrapper.sdk
 internal object UnsplashSdkConfig {
 
     private const val baseUrl = "https://api.unsplash.com"
+    const val OAUTH_URL = "https://unsplash.com/oauth/token"
 
     fun buildUrl(url: String) = "$baseUrl/$url"
 
