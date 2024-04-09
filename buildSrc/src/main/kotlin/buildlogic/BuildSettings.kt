4744b67a70cfe7fe0469bcfce95c463d23f904f2
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-04-09 01:28:46 -0300

    feat: fix color parsing

--- buildSrc/src/main/kotlin/buildlogic/BuildSettings.kt
+++ buildSrc/src/main/kotlin/buildlogic/BuildSettings.kt
@@ -2,7 +2,7 @@ package buildlogic
 
 object BuildSettings {
 
-    const val VERSION = "0.0.2"
+    const val VERSION = "0.0.3"
     const val GROUP = "io.github.alaksion"
     const val ARTIFACT = "unsplash-wrapper"
 
--- shared/src/commonMain/kotlin/io/github/alaksion/unsplashwrapper/platform/color/UnsplashColor.kt
+++ shared/src/commonMain/kotlin/io/github/alaksion/unsplashwrapper/platform/color/UnsplashColor.kt
@@ -5,12 +5,27 @@ import androidx.compose.ui.graphics.Color
 data class UnsplashColor(
     val hex: String
 ) {
-    val composeColor: Color
-        get() {
-            return try {
-                Color(hex.replace("#", "0xff").toInt())
-            } catch (e: Throwable) {
-                Color.White
-            }
-        }
+    val composeColor: Color = try {
+        Color(parseColor(hex))
+    } catch (e: Throwable) {
+        Color.White
+    }
+}
+
+private fun parseColor(hexString: String): Int {
+    require(hexString.isNotEmpty())
+
+    if (hexString[0] != '#') {
+        throw IllegalArgumentException("Invalid ehx string")
+    }
+    // Use a long to avoid rollovers on #ffXXXXXX
+    var color = hexString.substring(1).toLong(16)
+    if (hexString.length == 7) {
+        // Set the alpha value
+        color = color or 0x00000000ff000000L
+    } else if (hexString.length != 9) {
+        throw IllegalArgumentException("Unknown color")
+    }
+    return color.toInt()
+
 }
